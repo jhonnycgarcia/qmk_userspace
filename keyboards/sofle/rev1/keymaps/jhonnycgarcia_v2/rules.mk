b628deac0ab3515c78f2f9b7669440bc2f34cee7
Jhonny <jhonnycgarcia@gmail.com>
2025-08-13 22:01:36 -0500

    Update Sofle keyboard keymaps and configuration
    - Bump userspace version to 1.1 and add new build targets for jhonnycgarcia and jhonnycgarcia_v2.
    - Introduce new keymap files for jhonnycgarcia and jhonnycgarcia_v2, including custom keycodes and layer definitions.
    - Add configuration files for both keymaps, enabling tri-layer support and RGB lighting.
    - Create README files for both keymaps detailing features and usage instructions.

---
+++ keyboards/sofle/rev1/keymaps/jhonnycgarcia_v2/rules.mk
@@ -0,0 +1,10 @@
+# Bootloader selection
+BOOTLOADER = caterina
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+CONSOLE_ENABLE = no
+RGBLIGHT_ENABLE = yes
+ENCODER_ENABLE = yes
+LTO_ENABLE = yes
+OLED_ENABLE = yes
+VIA_ENABLE = yes
